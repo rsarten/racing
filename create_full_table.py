bf5b3c521cbb420f3e57bab33ee98cd58f29b626
Rory <rorysarten@gmail.com>
2023-12-31 22:39:26 +1300

    update create_full_table to script

--- create_full_table.py
+++ create_full_table.py
@@ -1,13 +1,20 @@
+import sys
+
 import numpy as np
 import pandas as pd
 
 from src.export.connections import get_engine, get_connection
 
-password=""
-engine = get_engine(password)
-conn = get_connection(password)
+def create_full_table(password: str):
+    engine = get_engine(password)
+    conn = get_connection(password)
 
-with open('sql/all_tables.sql', 'r') as query:
-    all_tables = pd.read_sql_query(query.read(), conn)
+    with open('sql/all_tables.sql', 'r') as query:
+        all_tables = pd.read_sql_query(query.read(), conn)
 
-all_tables.to_csv("data/all_tables.csv", index=False)
+    print("table shape:", all_tables.shape)
+    all_tables.to_csv("data/all_tables.csv", index=False)
+
+if __name__ == "__main__":
+    password = sys.argv[1]
+    create_full_table(password)
